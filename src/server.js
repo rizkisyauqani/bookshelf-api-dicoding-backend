e1e1b12e374eb96222357797896358f0e2c9339f
Rizki Syauqani <rizki.syauqani04@gmail.com>
2024-04-02 01:29:28 +0700

    import routes from routes.js file and use it as route to any api request

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const Hapi = require("@hapi/hapi");
-const { start } = require("repl");
+const routes = require('./routes')
 
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
     host: "localhost",
   });
+
+  server.route(routes)
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
